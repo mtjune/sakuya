bdf7f778219da5f577c22d9c28b34d3fbd5eb0cb
mtjuney <mtjuney@gmail.com>
2015-12-08 23:36:19 +0900

    fix

--- sent_ir.py
+++ sent_ir.py
@@ -42,7 +42,7 @@ try:
             if pattern[1][i] <= time.time() - t_start:
                 break
         t_end = time.time()
-        pattern_conform[1].append(t_end - t_start)
+        # pattern_conform[1].append(t_end - t_start)
         t_start = t_end
 
         if i < len(pattern[0]):
@@ -51,7 +51,7 @@ try:
             while True:
                 if pattern[0][i] <= time.time() - t_start:
                     break
-            pattern_conform[0].append(t_end - t_start)
+            # pattern_conform[0].append(t_end - t_start)
             t_start = t_end
 
 
@@ -65,28 +65,28 @@ finally:
     print("cleanup")
 
 
-pattern_error = {1:[], 0:[]}
+# pattern_error = {1:[], 0:[]}
 
-error_max1 = 0.
-error_max0 = 0.
+# error_max1 = 0.
+# error_max0 = 0.
 
-for i in range(len(pattern[1])):
-    tmp = abs(pattern_conform[1][i] - pattern[1][i])
-    pattern_error[1].append(tmp)
-    if tmp > error_max1:
-        error_max1 = tmp
-
-for i in range(len(pattern[0])):
-    tmp = abs(pattern_conform[0][i] - pattern[0][i])
-    pattern_error[0].append(tmp)
-    if tmp > error_max0:
-        error_max0 = tmp
-
-print("1", error_max1)
-print("0", error_max0)
-
-
-
-with open("ir_error.yml", "w") as f:
-    yaml.dump(pattern_error, f)
-print("saved")
+# for i in range(len(pattern[1])):
+#     tmp = abs(pattern_conform[1][i] - pattern[1][i])
+#     pattern_error[1].append(tmp)
+#     if tmp > error_max1:
+#         error_max1 = tmp
+#
+# for i in range(len(pattern[0])):
+#     tmp = abs(pattern_conform[0][i] - pattern[0][i])
+#     pattern_error[0].append(tmp)
+#     if tmp > error_max0:
+#         error_max0 = tmp
+#
+# print("1", error_max1)
+# print("0", error_max0)
+#
+#
+#
+# with open("ir_error.yml", "w") as f:
+#     yaml.dump(pattern_error, f)
+# print("saved")

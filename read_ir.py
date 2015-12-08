9e49d9674e72ee24f9ebb144fc6eb39354e5b098
mtjuney <mtjuney@gmail.com>
2015-12-08 20:21:08 +0900

    fix

--- read_ir.py
+++ read_ir.py
@@ -1,19 +1,28 @@
 
 import RPi.GPIO as GPIO
 import time
+import yaml
 
-INPUT_PIN = 4
-OUTPUT_PIN = 11
+INPUT_PIN = 18
+OUTPUT_PIN = 12
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+# GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+GPIO.setup(INPUT_PIN, GPIO.IN)
 
 try:
+    # while True:
+    #     GPIO.output(OUTPUT_PIN, True)
+    #     time.sleep(5)
+    #     GPIO.output(OUTPUT_PIN, False)
+    #     time.sleep(5)
+
     while True:
-        GPIO.output(OUTPUT_PIN, True)
+
+        signal = GPIO.input(INPUT_PIN)
+        print(signal)
         time.sleep(5)
-        GPIO.output(OUTPUT_PIN, False)
-        time.sleep(5)
+
 
 except Exception:
     print("error")
